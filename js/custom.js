2283c1224123d59b07b769c36ddc9296a439734e
Radek <polishman19@gmail.com>
2014-04-27 13:25:36 +0200

    Cloned page from student's account

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+    var url = $('#contact-form').attr('action');
+    //$("#contact-form").ajaxForm({url: url, type: 'post'});
+    $("#submit-btn").click(function(e) {
+        $('#submit-btn').addClass('hidden');
+        $('#progress-bar').removeClass('hidden');
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $("#contact-form").serialize(), // serializes the form's elements.
+            success: function(data)
+            {
+                $('#progress-bar').addClass('hidden');
+                $('#submit-btn').removeClass('hidden');
+
+                if (data.result) {
+                    $('#success-alert').removeClass('hidden');
+                }
+                else {
+                    $('#error-alert').removeClass('hidden');
+                }
+            }
+        });
+
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+    });
+});
